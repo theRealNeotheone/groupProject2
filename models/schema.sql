cb7b79d8ca98e400945c3122ec666ef1474538e9
Matthew Bower <mttbwr91@gmail.com>
2018-09-17 08:32:39 -0700

    updated home page

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,13 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS group2db;
+CREATE DATABASE group2db;
+USE group2db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE askanything (
+    id INT NOT NULL AUTO_INCREMENT,
+    question VARCHAR(125) NULL,
+    answer VARCHAR,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO askanything (question, answer)
+VALUES("What is node.js?", "Node.js is an open-source, cross-platform JavaScript run-time environment that executes JavaScript code outside of a browser.");
